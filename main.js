9bcb33caa44ccb3d849d223564424102bbb1a01a
jeremy <jerimonski@gmail.com>
2024-08-06 22:17:46 -0400

    Planteo de algoritmo paso a paso, se excluye uso de css hasta finalizar con JS

---
+++ main.js
@@ -0,0 +1,19 @@
+const cash_customer = document.getElementById("cashMoney");
+console.log(cash_customer.value);
+const total = 2 + 2;
+console.log(total);
+/*   hacerlo de la siguiente manera con un appendChild
+    <li>`${change_in_drawer}`</li>
+    y asi para todos los demas, para que se vayan actualizando 
+    sin necestidad de crear un elemento en html 
+     <ul>
+                <li>Pennies: $1.01</li>
+                <li>Nickels: $2.05</li>
+                <li>Dimes: $3.1</li>
+                <li>Quarters: $4.25</li>
+                <li>Ones: $90</li>
+                <li>Fives: $55</li>
+                <li>Tens: $20</li>
+                <li>Twenties: $60</li>
+                <li>Hundreds: $100</li>
+            </ul> */
